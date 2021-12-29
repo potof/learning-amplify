380ab624bdfe6599f465c376fe63ae6b5504f692
potof <slptmac@gmail.com>
2021-12-29 20:43:16 +0900

    推移グラフ表示追加

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,7 +9,33 @@
   <h2>内訳</h2>
   <PieChart :chartData="pieData" />
 
-  <h2>List</h2>
+  <h2>List groupby</h2>
+  <table>
+    <thead>
+      <tr>
+        <th>author</th>
+        <th>page</th>
+        <th>count</th>
+      </tr>
+    </thead>
+    <tbody>
+      <div v-for="item in groupByYearAll" :key="item.id">
+        <tr>
+          <td>
+            {{ item.year }}
+          </td>
+          <td>
+            {{ item.page }}
+          </td>
+          <td>
+            {{ item.count }}
+          </td>
+        </tr>
+      </div>
+    </tbody>
+  </table>
+
+  <!-- <h2>List</h2>
   <table>
     <thead>
       <tr>
@@ -33,7 +59,7 @@
         </tr>
       </div>
     </tbody>
-  </table>
+  </table> -->
 </template>
 
 <script lang="ts">
@@ -41,40 +67,88 @@ import { API } from "aws-amplify";
 import { listBooks } from "../graphql/queries";
 
 import { Chart, ChartData, registerables } from "chart.js";
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
 import { PieChart, LineChart } from "vue-chart-3";
 Chart.register(...registerables);
 
 export default defineComponent({
   name: "Test",
-  data() {
-    return {
-      title: "",
-      readDate: "",
-      page: "",
-      books: [],
-    };
-  },
   components: {
     PieChart,
     LineChart,
   },
-  created() {
-    this.getBooks();
-  },
   setup() {
-    const lineData: ChartData<"line"> = {
-      labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul"],
-      datasets: [
-        {
-          label: "My First Dataset",
-          data: [65, 59, 80, 81, 56, 55, 40],
-          fill: true,
-          borderColor: "rgb(75, 192, 192)",
-          tension: 0.1,
+    const books = ref([]);
+    const groupByYearAll = ref();
+    const groupByYearHearder = ref([]);
+    const groupByYearCount = ref([]);
+    const lineData = ref();
+
+    const load = async () => {
+      console.log("hogehoge");
+
+      // AppSync 経由でデータ取得
+      const booksapi = await API.graphql({
+        query: listBooks,
+        variables: {
+          limit: 1000,
         },
-      ],
+      });
+
+      // Amplify コードが型推論できずにエラーになるので包んであげているけどやりたいことは books.value への代入
+      ((booksapi: any) => {
+        books.value = booksapi.data.listBooks.items;
+      })(booksapi);
+      // };
+
+      // 日付の折れ線グラフを表示するために、年で集計する
+      // const groupByYear = () => {
+      const groupBy = books.value.reduce((result: any, current: any) => {
+        const element = result.find(
+          (value: any) => value.year === current.readDate.slice(0, 4)
+        );
+
+        if (element) {
+          element.count++;
+          element.page += current.page;
+        } else {
+          result.push({
+            year: current.readDate.slice(0, 4),
+            count: 1,
+            page: current.page,
+          });
+        }
+
+        return result;
+      }, []); //初期値は[]
+
+      groupBy.sort((a: any, b: any) => a.year - b.year);
+      groupByYearHearder.value = groupBy.map((el: any) => el.year);
+      groupByYearCount.value = groupBy.map((el: any) => el.count);
+
+      groupByYearAll.value = groupBy;
+
+      console.log("fugafugaaa");
+
+      lineData.value = {
+        labels: groupByYearHearder.value,
+        datasets: [
+          {
+            label: "冊数",
+            data: groupByYearCount.value,
+            fill: true,
+            borderColor: "rgb(75, 192, 192)",
+            tension: 0.1,
+          },
+        ],
+      };
     };
+
+    load();
+    // groupByYear();
+    console.log("setup");
+    console.log(groupByYearHearder.value);
+    console.log(groupByYearCount.value);
 
     const pieData: ChartData<"pie"> = {
       labels: ["Red", "Blue", "Yellow"],
@@ -91,23 +165,7 @@ export default defineComponent({
         },
       ],
     };
-    return { pieData, lineData };
-  },
-
-  methods: {
-    async getBooks() {
-      const books = await API.graphql({
-        query: listBooks,
-        variables: {
-          limit: 1000,
-        },
-      });
-      console.log("aaa");
-      console.log(books);
-      ((books: any) => {
-        this.books = books.data.listBooks.items;
-      })(books);
-    },
+    return { pieData, lineData, books, groupByYearAll };
   },
 });
 </script>
